33b54a7285ba383b3e7459d021c23ab3cfc1bf02
Shaurya Kumar <63877755+kshau@users.noreply.github.com>
2022-10-12 20:56:52 -0400

    Add files via upload

--- js/command/RollCommand.js
+++ js/command/RollCommand.js
@@ -12,7 +12,7 @@ class RollCommand extends Command {
     static description = "Rolls a die :game_die: | __<sides>__";
 
     static call(args, data, token) {
-
+        
         var sides = (args[0] == undefined) ? (6) : (args[0]);
 
         if (isNaN(sides)) {
@@ -21,7 +21,7 @@ class RollCommand extends Command {
 
         var result = Math.round(Math.random() * (sides - 1)) + 1;
         
-        MessageSender.reply(data.id, `**${result}!** :game_die:`, token, data.channel_id);
+        MessageSender.reply(data.id, `**${result.toLocaleString("en-US")}!** :game_die:`, token, data.channel_id);
 
     }
 
